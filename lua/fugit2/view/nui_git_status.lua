07c031d2741c8e7137bdb5dd76cce4c2cd81b07f
SuperBo <supernbo@gmail.com>
2023-11-24 14:52:55 +0700

    fix: fix behind icon color

--- lua/fugit2/view/nui_git_status.lua
+++ lua/fugit2/view/nui_git_status.lua
@@ -414,7 +414,7 @@ function NuiGitStatus:update()
         upstream_line:append(string.format("  %d", git_status.upstream.ahead), "Fugit2Count")
       end
       if git_status.upstream.behind > 0 then
-        upstream_line:append(string.format("  %d", git_status.upstream.behind), "Fugit2Count")
+        upstream_line:append(string.format("  %d", git_status.upstream.behind), "Fugit2Count")
       end
 
       upstream_line:append(" " .. git_status.upstream.message)
