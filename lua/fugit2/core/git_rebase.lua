b3dadd08038d04f340775fb6041ed2d280786387
SuperBo <supernbo@gmail.com>
2024-11-11 14:07:07 +0700

    wip: git rebase

---
+++ lua/fugit2/core/git_rebase.lua
@@ -0,0 +1,9 @@
+---Module contains reimplementation of git rebase
+---Extends libgit2 git rebase with support for
+---SQUASH AND FIXUP
+
+
+---@module "fugit2.core.git_rebase"
+local M = {}
+
+return M
