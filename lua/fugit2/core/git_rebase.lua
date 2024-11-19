1380e39bbb7f4b62e567e77d657910f2bb1f98bf
SuperBo <supernbo@gmail.com>
2024-11-19 08:17:57 +0700

    wip: git rebase 2

--- lua/fugit2/core/git_rebase.lua
+++ lua/fugit2/core/git_rebase.lua
@@ -6,4 +6,27 @@
 ---@module "fugit2.core.git_rebase"
 local M = {}
 
+---@enum FUGIT2_GIT_REBASE_ACTION
+M.GIT_REBASE_ACTION = {
+  PICK   = 0,
+  REWORD = 1,
+  EDIT   = 2,
+  SQUASH = 3,
+  FIXUP  = 4,
+  EXEC   = 5,
+  DROP   = 6,
+  BREAK  = 7,
+  BASE   = 8,
+}
+
+
+-- Fugit2 Git In-memory rebase
+---@class Fugit2GitRebase
+---@field repo GitRepository
+---@field index GitIndex In memory index
+---@field commit GitCommit? Last commit
+---@field onto_id GitObjectId
+---@field onto_name string
+---@field operations FUGIT2_GIT_REBASE_ACTION[]
+
 return M
