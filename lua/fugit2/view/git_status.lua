SuperBo/fugit2.nvim

commit 62040f787a120cb31f9ee00f86b20f0dab734047

feat: add ssh signing reword

lua/fugit2/view/git_status.lua
@@ -824,6 +824,26 @@ function GitStatus:read_config()
   return config
 end
 
+-- Read gpg config use_ssh and keyid
+---@return boolean use_ssh
+---@return string? keyid
+function GitStatus:read_gpg_config()
+  local config = self:read_config()
+  if not config then
+    return false, nil
+  end
+
+  local keyid = config:get_string "user.signingkey" or nil
+  local use_ssh = (config:get_string "gpg.format" == "ssh")
+
+  if not use_ssh and self._git.signature then
+    -- get committer info as key id
+    keyid = tostring(self._git.signature)
+  end
+
+  return use_ssh, keyid
+end
+
 -- Updates git status.
 ---@overload fun()
 function GitStatus:update()
@@ -1245,11 +1265,10 @@ function GitStatus:_git_create_commit(message, args)
         result.err = err
       else
         -- create commit with gpg sign
-        local config = self:read_config()
-        local keyid = config and config:get_string "user.signingkey" or nil
+        local use_ssh, keyid = self:read_gpg_config()
         local err_msg
         commit_id, err, err_msg =
-          git_gpg.create_commit_gpg(self.repo, self.index, self._git.signature, prettified, keyid)
+          git_gpg.create_commit_gpg(self.repo, self.index, self._git.signature, prettified, use_ssh, keyid)
         result.commit_id = commit_id
         result.err = err
         result.message = err_msg
@@ -1259,7 +1278,7 @@ function GitStatus:_git_create_commit(message, args)
     -- callback func, called when finished
     async_utils.scheduler(function()
       if result.commit_id then
-        notifier.info(string.format("New %s commit %s", gpg_sign and "signed" or "", result.commit_id:tostring(8)))
+        notifier.info(string.format("New %scommit %s", gpg_sign and "signed " or "", result.commit_id:tostring(8)))
         self:hide_input(true)
         self:update()
         self:render()
@@ -1283,9 +1302,8 @@ function GitStatus:_git_extend_commit(args)
     commit_id, err = self.repo:amend_extend(self.index)
   else
     -- extend commit with gpg sign
-    local config = self:read_config()
-    local keyid = config and config:get_string "user.signingkey" or nil
-    commit_id, err, err_msg = git_gpg.extend_commit_gpg(self.repo, self.index, keyid)
+    local use_ssh, keyid = self:read_gpg_config()
+    commit_id, err, err_msg = git_gpg.extend_commit_gpg(self.repo, self.index, use_ssh, keyid)
   end
 
   if commit_id then
@@ -1322,9 +1340,8 @@ function GitStatus:_git_reword_commit(message, args)
     commit_id, err = self.repo:amend_reword(signature, prettified)
   else
     -- reword commit with gpg sign
-    local config = self:read_config()
-    local keyid = config and config:get_string "user.signingkey" or nil
-    commit_id, err, err_msg = git_gpg.reword_commit_gpg(self.repo, signature, prettified, keyid)
+    local use_ssh, keyid = self:read_gpg_config()
+    commit_id, err, err_msg = git_gpg.reword_commit_gpg(self.repo, signature, prettified, use_ssh, keyid)
   end
 
   if commit_id then
@@ -1363,9 +1380,8 @@ function GitStatus:_git_amend_commit(message, args)
   if not gpg_sign then
     commit_id, err = self.repo:amend(self.index, self._git.signature, prettified)
   else
-    local config = self:read_config()
-    local keyid = config and config:get_string "user.signingkey" or nil
-    commit_id, err, err_msg = git_gpg.amend_commit_gpg(self.repo, self.index, signature, prettified, keyid)
+    local use_ssh, keyid = self:read_gpg_config()
+    commit_id, err, err_msg = git_gpg.amend_commit_gpg(self.repo, self.index, signature, prettified, use_ssh, keyid)
   end
 
   if commit_id then
@@ -1679,7 +1695,8 @@ function GitStatus:_init_branch_menu()
           args = {
             "git_branches",
             "cwd=" .. vim.fn.fnameescape(self._git.path),
-        }}
+          },
+        }
       elseif vim.fn.exists ":FzfLua" > 0 then
         self:unmount()
         vim.cmd {
@@ -1687,7 +1704,7 @@ function GitStatus:_init_branch_menu()
           args = {
             "git_branches",
             "cwd=" .. vim.fn.fnameescape(self._git.path),
-          }
+          },
         }
       else
         notifier.error "No Telescope or FzfLua found!"

lua/fugit2/core/git_gpg.lua
@@ -1,5 +1,7 @@
 ---Module contains libgit2 with gpgme integration
 
+local PlenaryJob = require "plenary.job"
+
 local gpgme = require "fugit2.gpgme"
 local utils = require "fugit2.utils"
 
@@ -34,19 +36,14 @@ local function create_gpgme_context(keyid)
 end
 
 ---@param repo GitRepository
----@param context GPGmeContext
 ---@param commit_content string
+---@param gpg_sign string
 ---@param msg string
 ---@return GitObjectId?
 ---@return integer err
 ---@return string err_msg
-local function create_commit_with_context(repo, context, commit_content, msg)
-  local gpg_sign, err = gpgme.sign_string_detach(context, commit_content)
-  if not gpg_sign then
-    return nil, err, "Failed to sign commit"
-  end
-
-  local commit_id, head
+local function create_commit_with_sign(repo, commit_content, gpg_sign, msg)
+  local commit_id, head, err
   commit_id, err = repo:create_commit_with_signature(commit_content, gpg_sign, nil)
   if not commit_id then
     return nil, err, "Failed to create commit with sign"
@@ -65,28 +62,83 @@ local function create_commit_with_context(repo, context, commit_content, msg)
   return commit_id, 0, ""
 end
 
+---@param repo GitRepository
+---@param context GPGmeContext
+---@param commit_content string
+---@param msg string
+---@return GitObjectId?
+---@return integer err
+---@return string err_msg
+local function create_commit_with_context(repo, context, commit_content, msg)
+  local gpg_sign, err = gpgme.sign_string_detach(context, commit_content)
+  if not gpg_sign then
+    return nil, err, "Failed to sign commit"
+  end
+
+  return create_commit_with_sign(repo, commit_content, gpg_sign, msg)
+end
+
+---@param repo GitRepository
+---@param commit_content string
+---@param msg string
+---@param keyid string
+---@return GitObjectId?
+---@return integer err
+---@return string err_msg
+local function create_commit_with_ssh(repo, commit_content, msg, keyid)
+  local job = PlenaryJob:new {
+    command = "ssh-keygen",
+    args = { "-Y", "sign", "-n", "git", "-f", keyid },
+    enable_recording = true,
+    writer = commit_content,
+  }
+  local result, err = job:sync(2000, 200) -- wait upto 2 seconds
+  if err ~= 0 then
+    result = job:stderr_result()
+    local stderr = #result > 0 and result[1] or ""
+    return nil, err, "Failed to sign commit with ssh, " .. stderr
+  end
+
+  local ssh_sign = table.concat(result, "\n")
+  return create_commit_with_sign(repo, commit_content, ssh_sign, msg)
+end
+
 -- Creates commit with gpg signing
 ---@param repo GitRepository
 ---@param index GitIndex
 ---@param signature GitSignature
 ---@param msg string commit message
+---@param use_ssh boolean,
 ---@param keyid string? signing key id
 ---@return GitObjectId?
 ---@return integer err_code
 ---@return string err_msg
-function M.create_commit_gpg(repo, index, signature, msg, keyid)
-  local context, err, err_msg = create_gpgme_context(keyid)
-  if not context then
-    return nil, err, err_msg
+function M.create_commit_gpg(repo, index, signature, msg, use_ssh, keyid)
+  local context, commit_content, err, err_msg
+
+  if use_ssh and not keyid then
+    return nil, -1, "user.signingkey is need when gpg.format is ssh"
   end
 
-  local commit_content
+  if not use_ssh then
+    context, err, err_msg = create_gpgme_context(keyid)
+    if not context then
+      return nil, err, err_msg
+    end
+  end
+
   commit_content, err = repo:create_commit_content(index, signature, msg)
   if not commit_content then
-    return nil, err, "Failed to create commit content"
+    return nil, err, "Failed to create git commit content"
   end
 
-  return create_commit_with_context(repo, context, commit_content, msg)
+  if not use_ssh and context then
+    return create_commit_with_context(repo, context, commit_content, msg)
+  elseif keyid then
+    return create_commit_with_ssh(repo, commit_content, msg, keyid)
+  end
+
+  return nil, -1, "unknown error"
 end
 
 -- Amend commit with gpg signing
@@ -94,46 +146,62 @@ end
 ---@param index GitIndex?
 ---@param signature GitSignature?
 ---@param msg string? commit message
+---@param use_ssh boolean use ssh signing instead of gpg
 ---@param keyid string? signing key id
 ---@return GitObjectId?
 ---@return integer err_code
 ---@return string err_msg
-function M.amend_commit_gpg(repo, index, signature, msg, keyid)
-  local context, err, err_msg = create_gpgme_context(keyid)
-  if not context then
-    return nil, err, err_msg
+function M.amend_commit_gpg(repo, index, signature, msg, use_ssh, keyid)
+  local context, commit_content, err, err_msg
+
+  if use_ssh and not keyid then
+    return nil, -1, "user.signingkey is need when gpg.format is ssh"
   end
 
-  local commit_content
+  if not use_ssh then
+    context, err, err_msg = create_gpgme_context(keyid)
+    if not context then
+      return nil, err, err_msg
+    end
+  end
+
   commit_content, msg, err = repo:amend_commit_content(index, signature, signature, msg)
   if not commit_content or not msg then
-    return nil, err, "Failed to create commit content"
+    return nil, err, "Failed to create git commit content"
   end
 
-  return create_commit_with_context(repo, context, commit_content, msg)
+  if not use_ssh and context then
+    return create_commit_with_context(repo, context, commit_content, msg)
+  elseif keyid then
+    return create_commit_with_ssh(repo, commit_content, msg, keyid)
+  end
+
+  return nil, -1, "unknown error"
 end
 
 -- Rewords commit with gpg signing
 ---@param repo GitRepository
 ---@param signature GitSignature?
 ---@param msg string? commit message
+---@param use_ssh boolean use ssh signing instead of gpg
 ---@param keyid string? signing key id
 ---@return GitObjectId?
 ---@return integer err_code
 ---@return string err_msg
-function M.reword_commit_gpg(repo, signature, msg, keyid)
-  return M.amend_commit_gpg(repo, nil, signature, msg, keyid)
+function M.reword_commit_gpg(repo, signature, msg, use_ssh, keyid)
+  return M.amend_commit_gpg(repo, nil, signature, msg, use_ssh, keyid)
 end
 
 -- Extend commit with gpg signing
 ---@param repo GitRepository
 ---@param index GitIndex
+---@param use_ssh boolean use ssh signing instead of gpg
 ---@param keyid string? signing key id
 ---@return GitObjectId?
 ---@return integer err_code
 ---@return string err_msg
-function M.extend_commit_gpg(repo, index, keyid)
-  return M.amend_commit_gpg(repo, index, nil, nil, keyid)
+function M.extend_commit_gpg(repo, index, use_ssh, keyid)
+  return M.amend_commit_gpg(repo, index, nil, nil, use_ssh, keyid)
 end
 
 return M

commit 0bfac16c99d834f667f38f533f4b4c6574ee1bbe

fix(sign/gpg): keyid got overrided by signature
Cannot find the right key configured with ${user.signingkey}:

> [Fugit2] Failed to get gpg key ${user.name} <${user.email}>.
> Error code 16383

Fixes: 62040f7 ("feat: add ssh signing reword")

lua/fugit2/view/git_status.lua
@@ -817,7 +817,7 @@ function GitStatus:read_gpg_config()
   local keyid = config:get_string "user.signingkey" or nil
   local use_ssh = (config:get_string "gpg.format" == "ssh")
 
-  if not use_ssh and self._git.signature then
+  if not keyid and not use_ssh and self._git.signature then
     -- get committer info as key id
     keyid = tostring(self._git.signature)
   end